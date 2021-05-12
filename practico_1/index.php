f19a4e6e9985bfa6c4115e213e237eb57e44d317
thierryrodriguez <thierryrodriguez16@gmail.com>
2021-05-11 21:44:38 -0300

    11/5 (2) Ejercicio 1

--- practico_1/index.php
+++ practico_1/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Campatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practico 1</title>
@@ -11,7 +11,7 @@
 
 <body>
 <body bgcolor="LightCoral">
-<h2>Bienvenido al práctico 1</h2>
+<h1>Bienvenido al práctico 1</h1>
 <br><br>
     <?php
     require_once 'template/pag_principal.php';
