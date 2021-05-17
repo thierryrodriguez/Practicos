74a383ec59e74c72509d92ef2ffc0f171d6e561a
thierryrodriguez <thierryrodriguez16@gmail.com>
2021-05-17 20:22:40 -0300

    db

--- practico_1/ejercicios/ejercicio_2.php
+++ practico_1/ejercicios/ejercicio_2.php
@@ -7,18 +7,11 @@ echo "<p><h1>Ejercicio 2</h1><p><br>";
 ?>
 
 <body>
+<body bgcolor="7FEA92">
 
-<div class="color#ej_2"
         <?php
-        $nota;
-        foreach (range(1, 5) as $insuficiente) {
-        }
-        foreach (range(6, 9) as $suficiente) {
-        }
-        foreach (range(10, 11) as $muybueno) {
-        }
-        foreach (range(12, 12) as $excelente) {
+         
         }
         ?>
-    </div>
+</body>
 </body>
